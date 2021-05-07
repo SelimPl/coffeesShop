42fbce74c117e2728024e5320d7e192778fded58
Selimpl <selosdz@gmail.com>
2021-05-07 15:17:21 +0200

    DB, flavors

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -11,10 +12,10 @@ export class CoffeesController {
     }
     @Get('/')
     findAll(
-        @Query() paginationQuery
+        @Query() paginationQuery: PaginationQueryDto
     ){
-        // const { limit, offset } = paginationQuery;
-        return this.coffeService.findAll()
+       
+        return this.coffeService.findAll(paginationQuery)
     }
 ///
     @Get('/:id')
